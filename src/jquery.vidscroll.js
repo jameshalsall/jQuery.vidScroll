5e5da2ba4e7f5b6e49de6f887e9c01d667a156c9
James Halsall <james.t.halsall@googlemail.com>
2013-10-10 19:31:39 +0100

    Initial commit

---
+++ src/jquery.vidscroll.js
@@ -0,0 +1,17 @@
+/**
+ * jQuery vidScroll
+ *
+ * Experimental plugin for controlling video scrubber with the scroll
+ * position of the user's view port.
+ *
+ * @author  James Halsall <james.t.halsall@googlemail.com>
+ * @link    http://www.github.com/jameshalsall/jQuery.vidScroll
+ * @license GPL (http://www.gnu.org/licenses/gpl-3.0.html)
+ */
+$(function($) {
+
+    $.fn.vidScroll = function() {
+        //todo
+    }
+
+})(jQuery);
